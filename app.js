ba4dfad15fe97553de8f0c8bb3a908f923f1c86b
Aman Sethi <35420536+amansethi00@users.noreply.github.com>
2020-12-04 15:16:08 +0530

    feat:empty input value will alert user

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var textInp = document.querySelector("textarea");
 var btn = document.querySelector("#btn");
 var languages = document.querySelector("#languages");
 var serverUrl="https://api.funtranslations.com/translate/";
+var output = document.querySelector("#output")
 
 function errorHandler(error){
     alert("Sorry,Something is wrong.Please try again after some time!!");
@@ -10,6 +11,11 @@ function getTranslationUrl(selctor,text){
     return serverUrl+selctor+"?"+"text="+text;
 }
 function clickHandler(){
+    if(textInp.value === ""){
+        alert("Please enter some text and click submit!");
+    }
+    else{
     fetch(getTranslationUrl(languages.value,textInp.value)).then(response => response.json()).then(json =>output.innerText=json.contents.translated).catch(errorHandler);
+    }
 }
 btn.addEventListener('click',clickHandler);
